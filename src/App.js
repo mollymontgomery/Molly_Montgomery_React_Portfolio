456e895eaaee0747e039b334a0367febd57332b1
Molly Montgomery <Molly@mollys-air-2.lan>
2022-02-27 17:39:36 -0600

    basic layout for Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import Nav from './components/Nav';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
 
 function App() {
   return (
     <div>
       <main>
         <Nav></Nav>
+        <Footer></Footer>
       </main>
     </div>
   )
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <a href="https//github.com/mollymontgomery">Github</a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
