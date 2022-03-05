5e6f24ba5cbf0389ff8c76d1ea10291861c4cef4
Molly Montgomery <Molly@mollys-air-2.lan>
2022-03-04 19:24:19 -0600

    worked on the card set up for the portfolio

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,9 +2,18 @@ import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 
 function Project(props) {
+	const currentProject = useState(props)[0].projects;
+	const image = currentProject.image;
 	return (
-		<div className="card"
-	)
+		<div className="center">
+		<Card className="card-image bg-dark text-white">
+			<Card.Img src={require(`../../assets/images/${image}`)} className="card-image"/>
+			<Card.ImgOverlay>
+			<Card.Title>Project 1</Card.Title>
+			<Card.Text>This is a test</Card.Text>
+			</Card.ImgOverlay>
+		</Card>
+		</div>
 	);
 }
 
