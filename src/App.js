fa0ae8fc6a8c3c89eca1e418d93ab8553806f925
Molly Montgomery <Molly@mollys-air-2.lan>
2022-03-05 20:30:37 -0600

    worked on the about page, changed image out for headshot image

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
 
+
 function App() {
 	const [currentTab, setCurrentTab] = useState("about");
 
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,26 +1,40 @@
 import React from 'react';
 
+
+
 function About() {
   return (
     <section>
-      <div class="header">
+      <div>About Me</div>
+      <br></br>
+      <div className="header" id="about">
+        <div className="header">
           <img
-          src={require('../../assets/images/cover.jpeg')}
-          alt="about-me"
-          className="photo"
-        /><div>About Me</div>
+            src={require('../../assets/images/Headshot.jpg')}
+            alt="headshot"
+            className="photo"
+          />
+          <blockquote>
+              <p id="quote-text">
+                Hi, I'm Molly!
+              </p>
+              <p>
+                Full Stack Coding Bootcamp Graduate
+              </p>
+            </blockquote>
           </div>
+      </div>
 
       <div>
         <p>
-        I am working towards gaining the skills necessary to be a fullstack developer 
-        at the University of Minnesota Coding Bootcamp. Currently, I am working as a 
-        Facility Manager and have my Bachelor's degree from the University of Northern Iowa.
+          I am working towards gaining the skills necessary to be a fullstack developer
+          at the University of Minnesota Coding Bootcamp. Currently, I am working as a
+          Facility Manager and have my Bachelor's degree from the University of Northern Iowa.
         </p>
         <p>
-        I am located in the Kansas City metro area. In my free time I love to explore the city 
-        with my husband and two corgis. I spend a lot of time outdoors, adding to my plant collection,
-        and working on creative projects.
+          I am located in the Kansas City metro area. In my free time I love to explore the city
+          with my husband and two corgis. I spend a lot of time outdoors, adding to my plant collection,
+          and working on creative projects.
         </p>
       </div>
     </section>
