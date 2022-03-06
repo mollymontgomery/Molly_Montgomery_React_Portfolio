c91636c344e2f5769a4314051acf1a58f3892a77
Molly Montgomery <Molly@mollys-air-2.lan>
2022-03-05 23:42:49 -0600

    added new headshot photo so it could be a circle, and tried to fix footer

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,27 +5,23 @@ import React from 'react';
 function About() {
   return (
     <section>
-      <div>About Me</div>
-      <br></br>
       <div className="header" id="about">
         <div className="header">
-          <img
-            src={require('../../assets/images/Headshot.jpg')}
-            alt="headshot"
-            className="photo"
-          />
-          <blockquote>
-              <p id="quote-text">
-                Hi, I'm Molly!
-              </p>
-              <p>
-                Full Stack Coding Bootcamp Graduate
-              </p>
-            </blockquote>
-          </div>
+        <img
+              src={require('../../assets/images/IMG_4373.jpg')}
+              alt="headshot"
+              className="photo"
+            />
+          <blockquote id="quote">
+            <p id="quote-text">
+              Hi, I'm Molly! A Fullstack Coding Bootcamp Graduate.<br></br>
+            </p>
+          </blockquote>
+        </div>
       </div>
+<br></br>
 
-      <div>
+      <div className="aboutMe">
         <p>
           I am working towards gaining the skills necessary to be a fullstack developer
           at the University of Minnesota Coding Bootcamp. Currently, I am working as a
@@ -37,6 +33,8 @@ function About() {
           and working on creative projects.
         </p>
       </div>
+      <br></br>
+      <br></br>
     </section>
   );
 }
